af6121234e9bbb3a1a431a632adfc8b3fbfa4d68
prateekLambda <47247309+prateekLambda@users.noreply.github.com>
2020-04-29 05:55:37 +0530

    Update magicLeap.java

--- src/main/java/magicLeap.java
+++ src/main/java/magicLeap.java
@@ -42,14 +42,14 @@ public class magicLeap {
         capabilities.setCapability("version", version);
         capabilities.setCapability("platform", platform);
         capabilities.setCapability("build", "Mojave Dublin macmachines/safari/Tunneltest");
-        capabilities.setCapability("name", browser + "  " + version + "  " + platform+ "  "+ fixedIp );
+        capabilities.setCapability("name", browser + "  " + version + "  " + platform+ "  "+ fixedIp+"  "+geoLocation);
         capabilities.setCapability("network", true);
         capabilities.setCapability("visual", true);
         capabilities.setCapability("video", true);
         capabilities.setCapability("console", true);
         capabilities.setCapability("resolution", resolution);
-        capabilities.setCapability("tunnel", true);
-        capabilities.setCapability("tunnelName", "prateek");
+       // capabilities.setCapability("tunnel", true);
+        capabilities.setCapability("geoLocation", geoLocation);
         capabilities.setCapability("fixedIP", fixedIp);
 
         try {
@@ -138,6 +138,7 @@ public class magicLeap {
 
     }
 */
+    /*
     @Test(priority = 2)
     @org.testng.annotations.Parameters(value = {"browser", "version", "platform"})
     public void tunnelTest(String version, String platform, String browser) {
@@ -185,7 +186,7 @@ public class magicLeap {
         System.out.println("Time taken by the test to execute in local" + "    " + totaltimeforlocaltest);
 
     }
-
+*/
     @org.testng.annotations.Parameters(value = {"resolution", "browser", "version", "platform"})
     @Test(priority = 3)
     public void Resolution(String resolution, String version, String platform, String browser) {

97ba08aefd6706f8a4fa4f3e50d3e9c98251fcd9
prateekLambda <47247309+prateekLambda@users.noreply.github.com>
2020-11-20 19:19:55 +0530

    Update magicLeap.java

--- src/main/java/magicleapTesting/magicLeap.java
+++ src/main/java/magicleapTesting/magicLeap.java
@@ -86,7 +86,7 @@ public class magicLeap {
             capabilities.setCapability("version", "latest");
             //capabilities.setCapability("version", "latest" + "-" + i);
             capabilities.setCapability("platform", this.PlatformValue);
-            capabilities.setCapability("build", "Performance/Jenkins-11 " + "  " + this.PlatformValue + System.getProperty("BUILD_NUMBER"));
+            capabilities.setCapability("build", "Performance/Jenkins-12 " + "  " + this.PlatformValue + System.getProperty("BUILD_NUMBER"));
             capabilities.setCapability("name", this.TestName);
             capabilities.setCapability("resolution", this.ResolutionValueCap);
             capabilities.setCapability("console", true);

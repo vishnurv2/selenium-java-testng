2e245d8964d7c419c953d623808229c7d5dfdf50
Vishnu <31619747+vishnurv2@users.noreply.github.com>
2021-05-14 21:38:14 +0530

    Update UserTest.java

--- src/main/java/magicleapTesting/UserTest.java
+++ src/main/java/magicleapTesting/UserTest.java
@@ -1,32 +1,29 @@
 package magicleapTesting;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.util.ArrayList;
 
 public class UserTest {
 
     public void TestCase(RemoteWebDriver driver, String status) {
         try {
 
-            driver.get("https://161.202.167.2/funrep/");
-            Thread.sleep(100000);
-          //  driver.findElement(By.xpath("@//XCUIElementTypeAlert//XCUIElementTypeButton[@name='Allow']")).click();
-            System.out.println(driver.getTitle());
-            Thread.sleep(100000);
-
-
-         /*  LogEntries browserLOgs= driver.manage().logs().get(LogType.BROWSER);
-           System.out.println("Safari browser logs Enteries"+"---------------"+ browserLOgs);
-*/
-            //   for (int i = 0; i < 10; i++)
-            // {
-           /* Thread.sleep(5000);
+            driver.get("https://www.google.com/intl/en_in/earth/");
+            driver.findElement(By.cssSelector("body > div.ge-hero.ge-hero--home.ge-hero--text-clipping.ng-scope > div > div:nth-child(2) > div > div.ge-chapter__ctas.ge-desktop-only.ctas > div > a")).click();
+            Thread.sleep(5000);
+            driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "2");
             System.out.println(driver.getTitle());
 
-            driver.findElement(By.id("imgBtnRefreshCaptcha")).click();
-            Thread.sleep(15000);*/
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            status="failed";
+        }
 
-            //}
-          /*  //driver.findElement(By.className("teachablesnav-search--field")).sendKeys("hello");
+          /*
+            driver.findElement(By.className("teachablesnav-search--field")).sendKeys("hello");
             String val = "hello";
 
             Actions actions = new Actions(driver);
@@ -44,11 +41,6 @@ public class UserTest {
                 actions.build().perform();
             }*/
 
-        } catch (Exception T) {
-            System.out.println(T);
-            status = "failed";
-
-        }
         //   ((JavascriptExecutor) driver).executeScript("lambda-status=" + status);
 
     }
